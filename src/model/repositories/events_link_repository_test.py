c3fceeed55a43fd6a5aae0bd704e2f94475027dc
YUT4R0 <pedroyutaronakamura@gmail.com>
2025-02-22 20:21:35 -0400

    feat: finish controllers

--- src/model/repositories/events_link_repository_test.py
+++ src/model/repositories/events_link_repository_test.py
@@ -3,9 +3,17 @@ import pytest
 from src.model.repositories.events_link_repository import EventsLinkRepository
 
 
-def test_insert_subscriber():
+@pytest.mark.skip.name("Insert in db")
+def test_insert_event_link():
     event_id = 12
     subs_id = 18
     events_link_repo = EventsLinkRepository()
 
     events_link_repo.insert(event_id, subs_id)
+
+
+def test_select_event_link():
+    event_id = 12
+    subs_id = 18
+    events_link_repo = EventsLinkRepository()
+    events_link_repo.select_events_link(event_id, subs_id)
